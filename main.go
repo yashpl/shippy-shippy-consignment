1ac249e587cc02d9ee35d52c1edfe6ae23d877b0
Yash Paliwal <yashpaliwal14@gmail.com>
2019-05-02 19:17:14 +0530

    fixed typo

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 
 	// Import the generated protobuf code
-	pb "github.com/<YourUserName>/shippy-service-consignment/proto/consignment"
+	pb "github.com/yashpl/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
