3929d3df49e635c187dd3eba0b1c6bf84e1dce0f
Yash Paliwal <yashpaliwal14@gmail.com>
2019-05-07 16:40:05 +0530

    next step

--- main.go
+++ main.go
@@ -17,8 +17,13 @@ const (
 	port = ":50051"
 )
 
+const (
+	port = ":50051"
+)
+
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -35,6 +40,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -61,6 +71,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
+// GetConsignments -
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
 func main() {
 
 	repo := &Repository{}
@@ -77,11 +93,9 @@ func main() {
 	// protobuf definition.
 	pb.RegisterShippingServiceServer(s, &service{repo})
 
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-
 	log.Println("Running on port:", port)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
